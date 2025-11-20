e3c0e733cd6d3acbbfe123bf5e4680f5006918d4
lyla <demange.telecom@gmail.com>
2025-11-20 10:49:08 +0100

    server basis

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-server web adress :
+# Dilly Daily backend server
+
+## Server deployed adress :
+
 https://fastapi-example-da0l.onrender.com/
+
+## Run server locally
+
+((\*) = run only the first time)
+
+- **[run all following commands in a terminal open in dilly_daily_server folder]**
+- (on Linux : ) source .venv/bin/activate
+- (on Windows PowerShell) .venv\Scripts\Activate.ps1
+- (on Windows Bash) source .venv/Scripts/activate
+- (\*) python -m pip install --upgrade pip
+- (\*) echo "\*" > .venv/.gitignore
+- (\*) pip install "fastapi[standard]"
+- (\*) pip install -r requirements.txt
+- fastapi run main.py
+
+The server is now accessible at http://0.0.0.0:8000
--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from typing import Optional
-
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/ingredients")
+async def all_ingredients():
+    with open('data/ingredients.json') as json_data:
+        data = json.load(json_data,) 
+    return data
+
+@app.get("/recipes")
+async def all_recipes():
+    with open('data/recipes.json') as json_data:
+        data = json.load(json_data,) 
+    return data
+#@app.get("/recipes/{recipe_id}")
+#async def one_recipe():
+#    with open('data/recipes.json') as json_data:
+#        data = json.load(json_data,) 
+#    return data
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
